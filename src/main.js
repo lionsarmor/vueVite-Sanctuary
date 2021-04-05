5207fec8fc9652d1d765b98023f5a778bcbf640b
james weeks <james.m.weeks@gmail.com>
2021-04-04 19:01:28 -0700

    ini

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import x5GMaps from 'x5-gmaps'
+import './assets/tailwind.css'
 
-Vue.use(x5GMaps, 
-{ key: GOOGLE_KEY, libraries: ['places'] }
-)
 createApp(App).mount('#app')
