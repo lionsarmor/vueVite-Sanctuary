df5c3c84448f88c074c7c37bd35d0eaca04dbb44
james weeks <james.m.weeks@gmail.com>
2021-04-05 14:53:51 -0700

    finished video section

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="bg-white text-gray-600 body-font">
+<section class="bg-white sanctuary-font text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto flex flex-wrap">
     <div class="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
       <div class="w-full sm:p-4 px-4 mb-6">
