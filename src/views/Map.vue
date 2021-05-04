41fcf5f36202e7d95ab874125781e6ae9eac3198
james weeks <james.m.weeks@gmail.com>
2021-05-04 15:32:18 -0700

    updated map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,23 @@
 <template>
-<h1>DEJDQJWDJQWSQKSQSLQLWSQS:Q:W</h1>
+            <router-link to="/" class="back">
+            <a href="#" class="bg-vegan-green hover:bg-yellow-400 py-3 px-4 rounded-full text-lg text-white sanctuary-font font-bold uppercase">
+                Back
+            </a>
+            </router-link>
+<section class="bg-green sanctuary-font text-gray-600 body-font">
+<div class="center" id="map">
+<div class="map-responsive">
+<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1B20RYW0iHBKjp8A4m97pdxuUfU55aNGj" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</div> 
+</section>
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
   components: {
-    
   }
 }
 </script>
@@ -19,7 +28,51 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #ffffff;
   margin-top: 60px;
+}
+.map-rsponsive{
+
+    overflow:hidden;
+
+    padding-bottom: 66%;
+
+    position:relative;
+
+    height:0;
+
+}
+
+.map-responsive iframe{
+
+    left:0;
+
+    top:12vh;
+
+    height:100%;
+
+    width:100%;
+
+    position:absolute;
+
+}
+.back {
+  position: absolute;
+  margin-top: -35px;
+  left:1%;
 }
 </style>
